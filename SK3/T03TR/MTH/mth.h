8c2d3481a761b25db3ce5fe0ab892c730324946b
slava233 <60133201+slava233@users.noreply.github.com>
2021-12-23 18:50:36 +0300

    Upload triangulation project

---
+++ SK3/T03TR/MTH/mth.h
@@ -0,0 +1,13 @@
+/* FILE NAME   : mth.h
+ * PROGRAMMER  : SK3.
+ * LAST UPDATE : 20.07.2020.
+ */
+
+#ifndef __MTH_H_
+#define __MTH_H_
+
+#include "mth_vec2.h"
+
+#endif /* __MTH_H_ */
+
+/* END OF 'mth.h' FILE */
